740c601fda4bfffe368763bb868f3bc61a6af07c
Bogdan Salyp <bogdansalyp@gmail.com>
2019-09-02 09:53:35 +0300

    Added jsdoc; Renamed deepIterator to changeField

--- source/set.js
+++ source/set.js
@@ -1,13 +1,20 @@
 'use strict';
 
-const deepIterator = (target, arr, value) => {
+ /**
+ * Recursive function to insert value to the given object by the given path.
+ * @param {object} target The object to modify.
+ * @param {string} arr The path to the value to be changed.
+ * @param {object} value The value to insert.
+ * @returns {object} The modified object.
+ */
+const changeField = (target, arr, value) => {
     if (arr.length === 0) {
         return value;
     } else {
         const el = arr.shift();
         target[el] = (typeof target[el] === 'object') 
-            ? deepIterator(target[el], arr, value) 
-            : deepIterator({}, arr, value);
+            ? changeField(target[el], arr, value) 
+            : changeField({}, arr, value);
     }
     return target;
   }
@@ -25,5 +32,5 @@ const set = (obj, key, value) => {
     }
     let arr = key.split('.');
     arr.shift();
-    return deepIterator(obj, arr, value);
+    return changeField(obj, arr, value);
 }
